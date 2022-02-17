ce0c7317d8a084a235af3d620b0a5edb2c510ee2
Tanner <troxs690@gmail.com>
2022-02-16 18:20:21 -0600

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+
+function submitData(name, email) { 
+    const newData = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    "name": name,
+    "email": email
+  })}
+
+
+let body = document.querySelector('body')
+return fetch('http://localhost:3000/users', newData)
+.then(resp => resp.json())
+.then(resp => {
+  let p = document.createElement("p")
+   p.textContent = resp.id
+   body.appendChild(p)
+})
+  .catch(error => {
+    let p = document.createElement('p')
+    p.textContent = error.message
+   body.appendChild(p)
+
+  })}
